83be4f84a176a5c82b816233d84801cc99e4174a
mumbai admin panel <231066419+rajasthan925677-cmd@users.noreply.github.com>
2025-09-16 10:33:13 +0530

    Update firebase.js with env, modify packages, remove processJodiResultServices.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,14 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-
-apiKey: "AIzaSyDvd_GJAIONKx-RMfIXVQOjTU5CCfFdRE8",
-  authDomain: "mumbaimatka-9b2ba.firebaseapp.com",
-
-  projectId: "mumbaimatka-9b2ba",
-  storageBucket: "mumbaimatka-9b2ba.firebasestorage.app",
-  messagingSenderId: "607401586790",
-  appId: "1:607401586790:web:fc45724fb924e60eac7da7",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Firestore export
 export const db = getFirestore(app);
-
